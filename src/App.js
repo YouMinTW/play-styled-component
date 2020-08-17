9687801dd4abec06e99afd3b449325ae43d71bca
YouMinTW <m791213p@gmail.com>
2020-08-17 16:04:47 +0800

    play ground

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { css } from "styled-components";
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+const Counter = ({ className }) => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <Wrapper className={className}>
+      <div>
+        <h1>{count}</h1>
+
+        <Button primary onClick={() => setCount((pre) => pre + 1)}>
+          +1
+        </Button>
+        <Button onClick={() => setCount((pre) => pre - 1)}>-1</Button>
+      </div>
+    </Wrapper>
+  );
+};
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+const StyledCounter = styled(Counter)`
+  background-color: blue;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TomatoButton>123</TomatoButton>
+      <Counter />
+      <StyledCounter />
+      <Link>1111</Link>
+      <StyledLink>1111</StyledLink>
     </div>
   );
 }
-
+const Link = ({ children, className }) => {
+  return <a className={className}>{children}</a>;
+};
+const StyledLink = styled(Link)`
+  color: palevioletred;
+  font-weight: bold;
+`;
 export default App;
